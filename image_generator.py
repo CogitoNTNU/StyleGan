d9cf598e131535379f09dc4473ea4c073f47cf63
espenbfo <espenbfo@stud.ntnu.no>
2020-10-08 19:43:46 +0200

    Add adverserial model

--- image_generator.py
+++ image_generator.py
@@ -4,11 +4,13 @@ import numpy as np
 import random
 from data import load_images_from_hdf5
 
+#Før man kan kjøre denne må man kjøre load_and_convert_from_folder fra data.py, for å få bildene på riktig format.
 def image_generator(batch_size, data_folder):
     current_length = 0
     current_sets = []
     paths = os.listdir(data_folder)
     paths = list(filter(lambda x: x[-3:] == ".h5", paths))
+
     while True:
 
         random.shuffle(paths)
@@ -16,7 +18,6 @@ def image_generator(batch_size, data_folder):
         for path in paths:
             current_sets.append(load_images_from_hdf5(os.path.join(data_folder,path)))
             current_length += current_sets[-1].shape[0]
-            #print(current_length)
 
             if current_length >= batch_size:
                 last_set_length = current_sets[-1].shape[0]
