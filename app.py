9881527946d53d7bf253bfc579bdf2dfdc5e32bc
Andreas Gravrok <45593399+joulebit@users.noreply.github.com>
2020-10-29 20:49:51 +0100

    Latent space video change
    made simulation video for changes between latent spaces

--- app.py
+++ app.py
@@ -9,31 +9,31 @@ from tensorflow.keras.optimizers import Adam
 import numpy as np
 from models.generator import random_generator_input
 import cv2
+import glob
 IMG_SIZE = 512
 
 gen = models.generator.get_generator(latent_dim=512, channels=64, target_size=IMG_SIZE, latent_style_layers=2)
 gen.compile("adam","binary_crossentropy")
 
-pygame.init()
-w, h = 400, 400
-screen = pygame.display.set_mode((w, h))
-running = True
+# pygame.init()
+# w, h = 400, 400
+# screen = pygame.display.set_mode((w, h))
+# running = True
 
-smallfont = pygame.font.SysFont('Corbel', 30)
-nextText = smallfont.render('Generate new picture', True, (0, 0, 0))
-quitText = smallfont.render('X', True, (0, 0, 0))
+# smallfont = pygame.font.SysFont('Corbel', 30)
+# nextText = smallfont.render('Generate new picture', True, (0, 0, 0))
+# quitText = smallfont.render('X', True, (0, 0, 0))
 
 # Generate random nooise to be used for all generated pictures
 r_noise_input = random_generator_input(1, 512, IMG_SIZE)
 r_noise2 = r_noise_input.copy()
 r_noise2[1] = np.random.normal(size=(1,512))
 
-print(r_noise2[1])
 
 placeholder = r_noise_input.copy()
 
 counter = 0
-n = 100
+n = 1000
 
 def update_latent_vector():
     current_latent = (r_noise_input.copy()[1]*(n-counter) + r_noise2.copy()[1]*counter)/n
@@ -52,7 +52,7 @@ def updateImage():
     global r_noise2
     global n
     image = generate_image()
-    image = pygame.surfarray.make_surface(image)
+    #image = pygame.surfarray.make_surface(image)
 
     if counter <= n:
         counter += 1
@@ -62,9 +62,9 @@ def updateImage():
 
     print(counter)
 
-    rect = image.get_rect()
-    rect.center = w/2, h/2
-    return image, rect
+    # rect = image.get_rect()
+    # rect.center = w/2, h/2
+    return image#, rect
 
 
 def getNextButtonRect():
@@ -79,34 +79,61 @@ def getQuitButtonRect():
     return rect
 
 
-image, rect = updateImage()
+img_array = []
+img = updateImage()
+while counter <= n:
+    img = updateImage()
+    img_array.append(img)
 
-time_elapsed_since_last_action = 0
-clock = pygame.time.Clock()
-while running:
-    nextButtonRect = getNextButtonRect()
-    quitButtonRect = getQuitButtonRect()
-    for event in pygame.event.get():
-        if event.type == MOUSEBUTTONDOWN:
-            if nextButtonRect.collidepoint(event.pos):
-                image, rect = updateImage()
-            if quitButtonRect.collidepoint(event.pos):
-                running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                image, rect = updateImage()
+# for image in img_array:
+#     cv2.imshow('Color image', image)
+#     import time
+#     time.sleep(4)
+#     cv2.destroyAllWindows()
 
-    dt = clock.tick() 
-    time_elapsed_since_last_action += dt
-    if time_elapsed_since_last_action > 50:
-        image, rect = updateImage()
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, (512,512))
+ 
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
+
+
+# for img_index in range(len(img_array)):
+#     cv2.imshow('Color image', img_array[img_index])
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+
+
+
+# image, rect = updateImage()
+
+# time_elapsed_since_last_action = 0
+# clock = pygame.time.Clock()
+# while running:
+#     nextButtonRect = getNextButtonRect()
+#     quitButtonRect = getQuitButtonRect()
+#     for event in pygame.event.get():
+#         if event.type == MOUSEBUTTONDOWN:
+#             if nextButtonRect.collidepoint(event.pos):
+#                 image, rect = updateImage()
+#             if quitButtonRect.collidepoint(event.pos):
+#                 running = False
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_RETURN:
+#                 image, rect = updateImage()
+
+#     dt = clock.tick() 
+#     time_elapsed_since_last_action += dt
+#     if time_elapsed_since_last_action > 50:
+#         image, rect = updateImage()
     
-    screen.fill((0, 0, 0))
-    screen.blit(image, rect)
-    pygame.draw.rect(screen, (34, 139, 34), [68, h-45, 265, 28])
-    pygame.draw.rect(screen, (255, 0, 0), [w-40, h-45, 25, 27])
-    screen.blit(quitText, quitButtonRect)
-    screen.blit(nextText, nextButtonRect)
-    pygame.display.update()
+#     screen.fill((0, 0, 0))
+#     screen.blit(image, rect)
+#     pygame.draw.rect(screen, (34, 139, 34), [68, h-45, 265, 28])
+#     pygame.draw.rect(screen, (255, 0, 0), [w-40, h-45, 25, 27])
+#     screen.blit(quitText, quitButtonRect)
+#     screen.blit(nextText, nextButtonRect)
+#     pygame.display.update()
 
-pygame.quit()
+# pygame.quit()
