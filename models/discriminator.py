8d7ab8d8f39ff9514a1d64e3f8c39a7a777036a2
Tobias Christiani <tobiaschristiani@gmail.com>
2020-11-05 15:15:19 +0100

    Working on variable aspect ratio

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,156 +1,55 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, AveragePooling2D, Flatten, Add, LeakyReLU, Dropout
+from tensorflow.keras.layers import Dense, Conv2D, AveragePooling2D, Flatten, Add, LeakyReLU, Dropout, BatchNormalization
 from tensorflow.keras import Input, Model
 
-# def get_discriminator(img_size):
-#     inp = Input(shape=(img_size,img_size,3))
-#     discriminator = Conv2D(16, kernel_size=1, padding="same")(inp)
-#     discriminator = LeakyReLU(0.2)(discriminator)
 
+def get_resnet_discriminator(img_size=(64, 64), filters=16, dense_units=64):
 
-#     while img_size > 4:
-#         if (1024 * 16) / img_size >= 512:
-#             filter = 512
-#         else:
-#             filter = int((1024 * 16) / img_size)
-#         discriminator = box(discriminator, filter, (img_size, img_size, 3))
-#         img_size = img_size / 2
-
-
-#     discriminator = Conv2D(512, kernel_size=3,input_shape=(4, 4, 3), padding="same")(discriminator)
-#     discriminator = LeakyReLU(0.2)(discriminator)
-
-#     discriminator = Conv2D(512, kernel_size=4,input_shape=(4, 4, 3), padding="same")(discriminator)
-#     discriminator = LeakyReLU(0.2)(discriminator)
-
-#     discriminator = Flatten()(discriminator)
-
-#     discriminator = Dense(1, activation="sigmoid")(discriminator)
-
-#     return discriminator
-
-# def box(discriminator, filter, img_size):
-#     # conv_part = Conv2D(filter, kernel_size=3,input_shape=img_size, padding="same")(discriminator)
-#     # conv_part = LeakyReLU(0.2)(conv_part)
-#     if (filter < 512):
-#         filter *= 2
-#     conv_part = Conv2D(filter,strides=2, kernel_size=3,input_shape=img_size, padding="same")(conv_part)
-#     conv_part = LeakyReLU(0.2)(conv_part)
-#     downsampling_part = AveragePooling2D(pool_size=(2, 2))(discriminator)
-#     discriminator = Add()([conv_part, downsampling_part])
-#     return discriminator
-
-
-def get_resnet_discriminator(img_size, filters=16):
-
-    z = Input(shape=(img_size, img_size, 3))
+    img_width = img_size[0]
+    img_height = img_size[1]
+    
+    z = Input(shape=(img_width, img_height, 3))
 
     # fRGB 
     x = Conv2D(filters, kernel_size=1, padding="same")(z)
+    x = BatchNormalization()(x) # BatchNorm can also be added after activation 
     x = LeakyReLU(alpha=0.2)(x)
 
-    size = img_size
+    size = min(img_width, img_height)
     while size > 4:
         # Pass x through separate convolutional and downsampling blocks (resnet)
 
         # Convolution
         x1 = Conv2D(filters, kernel_size=3, padding="same", kernel_initializer="random_normal")(x)
+        x1 = BatchNormalization()(x1)
         x1 = LeakyReLU(alpha=0.2)(x1)
         x1 = Conv2D(filters, kernel_size=3, padding="same", kernel_initializer="random_normal")(x1)
+        x1 = BatchNormalization()(x1)
         x1 = LeakyReLU(alpha=0.2)(x1)
         x1 = AveragePooling2D(pool_size=(2, 2))(x1)
 
         # Downsampling
         x2 = AveragePooling2D(pool_size=(2, 2))(x)
         x2 = Conv2D(filters, kernel_size=1, kernel_initializer="random_normal", padding="same")(x2) # According to Fig. 7
+        x1 = BatchNormalization()(x1)
         x2 = LeakyReLU(alpha=0.2)(x2)
 
         # Add back to a single image
         x = Add()([x1, x2])
-
+        x = BatchNormalization()(x)
         size = size//2
 
     x = Flatten()(x)
     x = Dropout(rate=0.2)(x)
-    x = Dense(filters)(x)
+    x = Dense(dense_units)(x)
+    x = BatchNormalization()(x)
     x = LeakyReLU(alpha=0.2)(x)
     x = Dropout(rate=0.2)(x)
-    x = Dense(filters)(x)
+    x = Dense(dense_units)(x)
+    x = BatchNormalization()(x)
     x = LeakyReLU(alpha=0.2)(x)
     x = Dense(1, activation="sigmoid")(x)
 
     discriminator = tf.keras.Model(inputs=z, outputs=x, name="discriminator")
-    return discriminator
-
-
-def get_simple_discriminator(img_size, filters=16):
-
-    z = Input(shape=(img_size, img_size, 3))
-
-    # fRGB 
-    x = Conv2D(filters, kernel_size=1, padding="same")(z)
-    x = LeakyReLU(alpha=0.2)(x)
-
-    size = img_size
-    while size > 4:
-        # Simple version
-        x = Conv2D(filters, kernel_size=3, padding="same")(x)
-        x = LeakyReLU(alpha=0.2)(x)
-        x = Conv2D(filters, kernel_size=3, padding="same")(x)
-        x = LeakyReLU(alpha=0.2)(x)
-        x = AveragePooling2D(pool_size=(2, 2))(x)
-        size = size//2
-
-    x = Flatten()(x)
-    x = Dropout(rate=0.2)(x)
-    
-    x = Dense(4*4*filters)(x)
-    x = LeakyReLU(alpha=0.2)(x)
-    x = Dense(1, activation="sigmoid")(x)
-
-    discriminator = tf.keras.Model(inputs=z, outputs=x, name="discriminator")
-    return discriminator
-
-
-def get_discriminator(img_size):
-    discriminator = Sequential()
-
-    discriminator.add(Conv2D(16, kernel_size=1,
-                             input_shape=(img_size, img_size, 3), padding="same"))
-    discriminator.add(LeakyReLU(0.2))
-
-    while(img_size > 4):
-        if ((1024*16)/img_size >= 512):
-            filter = 512
-        else:
-            filter = int((1024*16)/img_size)
-        discriminator = box(discriminator, filter, (img_size, img_size, 3))
-        img_size = img_size/2
-
-    discriminator.add(Conv2D(512, kernel_size=3,
-                             input_shape=(4, 4, 3), padding="same"))
-    discriminator.add(LeakyReLU(0.2))
-
-    discriminator.add(Conv2D(512, kernel_size=4,
-                             input_shape=(4, 4, 3), padding="same"))
-    discriminator.add(LeakyReLU(0.2))
-
-    discriminator.add(Flatten())
-
-    discriminator.add(Dense(1, activation="sigmoid"))
-
-    return discriminator
-
-
-def box(discriminator, filter, img_size):
-    discriminator.add(Conv2D(filter, kernel_size=3,
-                             input_shape=img_size, padding="same"))
-    discriminator.add(LeakyReLU(0.2))
-    if (filter < 512):
-        filter *= 2
-    discriminator.add(Conv2D(filter, kernel_size=3,
-                             input_shape=img_size, padding="same"))
-    discriminator.add(LeakyReLU(0.2))
-    discriminator.add(AveragePooling2D(pool_size=(2, 2)))
     return discriminator
--- models/generator.py
+++ models/generator.py
@@ -33,8 +33,7 @@ def style_block(x, latent_in, noise_in, channels=64, latent_style_layers=2, upsa
         x = layers.UpSampling2D(size=(2, 2), interpolation="bilinear", name=f"upsampling_{name}")(x)
 
     # Standard 2D convolution with 3x3 kernel
-    x = layers.Conv2D(channels, kernel_size=(3, 3), padding="same", kernel_initializer="random_normal",
-                      bias_initializer="zeros", name=f"conv2d_{name}")(x)
+    x = layers.Conv2D(channels, kernel_size=(3, 3), padding="same", kernel_initializer="random_normal", bias_initializer="zeros", name=f"conv2d_{name}")(x)
     x = layers.LeakyReLU(alpha=0.2)(x)
 
     # Normalize to unit standard deviation in each output channel
@@ -42,141 +41,69 @@ def style_block(x, latent_in, noise_in, channels=64, latent_style_layers=2, upsa
 
     # Add noise
     # We use a single channel of standard normal noise that is broadcasted across the channels of the generated image
+    # TODO: Try different random noise for each channel
     # For each channel in the generated image we have a learnable scaling factor and bias parameter, implemented using a Conv2D layer with a kernel of size 1.
-    b = layers.Conv2D(channels, kernel_size=(1, 1), kernel_initializer="zeros", bias_initializer="zeros",
-                      name=f"broadcast_modulate_noise_{name}")(noise_in)
+    b = layers.Conv2D(channels, kernel_size=(1, 1), kernel_initializer="zeros", bias_initializer="zeros", name=f"broadcast_modulate_noise_{name}")(noise_in)
     x = layers.Add(name=f"add_noise_{name}")([b, x])
 
     return x
 
 
-def get_generator(latent_dim=64, channels=64, target_size=64, latent_style_layers=2):
-    num_upsamples = int(math.log2(target_size) - 2)
-    side_length = 4
+def get_skip_generator(start_size=(4,4), target_size=(64, 64), latent_dim=64, channels=64, latent_style_layers=2):
+    width = start_size[0]
+    height = start_size[1]
+    target_width = target_size[0]
 
     # Learnable constant image
     dummy_in = layers.Input(shape=(1,), name="dummy_in")
-    x = layers.Dense(side_length * side_length * channels, name="const_img", kernel_initializer="zeros",
-                     bias_initializer="random_normal")(dummy_in)
-    x = layers.Reshape((side_length, side_length, channels))(x)
-
-    # Latent input
-    latent_in = layers.Input(shape=(latent_dim,), name="latent_in")
-
-    noise_inputs = []
-    for i in range(num_upsamples):
-        # Style block without upsampling
-        noise_in = layers.Input(shape=(side_length, side_length, 1), name=f"noise_in_{side_length}x{side_length}")
-        noise_inputs.append(noise_in)
-        x = style_block(x, latent_in, noise_in, channels=channels, latent_style_layers=latent_style_layers,
-                        upsample=False, name=f"{side_length}x{side_length}")
-
-        # Style block with upsampling
-        side_length = 2 * side_length
-        noise_in = layers.Input(shape=(side_length, side_length, 1),
-                                name=f"noise_in_upsample_{side_length}x{side_length}")
-        noise_inputs.append(noise_in)
-        x = style_block(x, latent_in, noise_in, channels=channels, latent_style_layers=latent_style_layers,
-                        upsample=True, name=f"upsample_{side_length}x{side_length}")
-
-    # Convert feature maps to RGB image
-    x = layers.Conv2D(filters=3, kernel_size=(3, 3), kernel_initializer="random_normal", bias_initializer="zeros",
-                      activation="tanh", padding="same")(x)
-
-    generator = tf.keras.Model(inputs=[dummy_in, latent_in] + noise_inputs, outputs=x, name="generator")
-    return generator
-
-def get_skip_generator(latent_dim=64, channels=64, target_size=64, latent_style_layers=2):
-    num_upsamples = int(math.log2(target_size) - 2)
-    side_length = 4
-
-    # Learnable constant image
-    dummy_in = layers.Input(shape=(1,), name="dummy_in")
-    x = layers.Dense(side_length * side_length * channels, name="const_img", kernel_initializer="zeros",
-                     bias_initializer="random_normal")(dummy_in)
-    x = layers.Reshape((side_length, side_length, channels))(x)
+    x = layers.Dense(width * height * channels, name="const_img", kernel_initializer="zeros", bias_initializer="random_normal")(dummy_in)
+    x = layers.Reshape((width, height, channels))(x)
 
     # RGB output
-    y = layers.Conv2D(filters=3, kernel_size=(3, 3), kernel_initializer="random_normal", bias_initializer="zeros",
-                      activation="tanh", padding="same")(x)
+    y = layers.Conv2D(filters=3, kernel_size=(3, 3), kernel_initializer="random_normal", bias_initializer="zeros", activation="tanh", padding="same")(x)
     
     # Latent input
     latent_in = layers.Input(shape=(latent_dim,), name="latent_in")
 
     noise_inputs = []
-    for i in range(num_upsamples):
+    while width < target_width:
         # Style block without upsampling
-        noise_in = layers.Input(shape=(side_length, side_length, 1), name=f"noise_in_{side_length}x{side_length}")
+        noise_in = layers.Input(shape=(width, height, 1), name=f"noise_in_{width}x{height}")
         noise_inputs.append(noise_in)
-        x = style_block(x, latent_in, noise_in, channels=channels, latent_style_layers=latent_style_layers,
-                        upsample=False, name=f"{side_length}x{side_length}")
+        x = style_block(x, latent_in, noise_in, channels=channels, latent_style_layers=latent_style_layers, upsample=False, name=f"{width}x{height}")
 
         # Style block with upsampling
-        side_length = 2 * side_length
-        noise_in = layers.Input(shape=(side_length, side_length, 1),
-                                name=f"noise_in_upsample_{side_length}x{side_length}")
+        width = 2*width
+        height = 2*height
+        noise_in = layers.Input(shape=(width, height, 1), name=f"noise_in_upsample_{width}x{height}")
         noise_inputs.append(noise_in)
-        x = style_block(x, latent_in, noise_in, channels=channels, latent_style_layers=latent_style_layers,
-                        upsample=True, name=f"upsample_{side_length}x{side_length}")
+        x = style_block(x, latent_in, noise_in, channels=channels, latent_style_layers=latent_style_layers, upsample=True, name=f"upsample_{width}x{height}")
 
         # Convert deep image to RGB
-        z = layers.Conv2D(filters=3, kernel_size=(3, 3), kernel_initializer="random_normal", bias_initializer="zeros",
-                    activation="tanh", padding="same", name=f"to_rgb_{side_length}x{side_length}")(x)
+        z = layers.Conv2D(filters=3, kernel_size=(3, 3), kernel_initializer="random_normal", bias_initializer="zeros", activation="tanh", padding="same", name=f"to_rgb_{width}x{height}")(x)
 
-        # Add deep RGB to upsampled current RGB
-        y = layers.UpSampling2D(size=(2, 2), interpolation="bilinear", name=f"rgb_upsampling_{side_length}x{side_length}")(y)
-        y = layers.Add(name=f"add_deep_rgb_{side_length}x{side_length}")([y, z])
+        # Upsample current RGB and add deep RGB
+        y = layers.UpSampling2D(size=(2, 2), interpolation="bilinear", name=f"rgb_upsampling_{width}x{height}")(y)
+        y = layers.Add(name=f"add_deep_rgb_{width}x{height}")([y, z])
 
     generator = tf.keras.Model(inputs=[dummy_in, latent_in] + noise_inputs, outputs=y, name="generator")
     return generator
 
-def get_dropout_generator(latent_dim=64, channels=64, target_size=64):
-    side_length = 4
-    latent_in = layers.Input(shape=(latent_dim,), name="latent_in")
-    x = layers.Dense(4*4*channels)(latent_in)
-    x = layers.LeakyReLU(alpha=0.2)(x)
-    x = layers.Reshape((side_length, side_length, channels))(x)
-
-    dropout_inputs = []
-    while side_length < target_size:
-        # 2x Conv2D
-        x = layers.Conv2D(filters=channels, kernel_size=(3,3), padding="same")(x)
-        x = layers.LeakyReLU(alpha=0.2)(x)
-        x = layers.Conv2D(filters=channels, kernel_size=(3,3), padding="same")(x)
-        x = layers.LeakyReLU(alpha=0.2)(x)
-
-        # Dropout entire channels
-        s = layers.Input(shape=(channels,))
-        dropout_inputs.append(s)
-        s = layers.Reshape((1,1,channels))(s)
-        x = layers.Lambda(scale_channels)([s, x])
-        
-        # Upsampling
-        x = layers.UpSampling2D(size=(2, 2), interpolation="bilinear")(x)
-        side_length = 2*side_length
-
-    x = layers.Conv2D(filters=3, kernel_size=(3,3), padding="same")(x)
-
-    generator = tf.keras.Model(inputs=[latent_in] + dropout_inputs, outputs=x, name="generator")
-    return generator
-
-
-def random_dropout_input(batch_size, latent_size, channels, img_size, alpha=0.2):
-    latent_noise = np.random.normal(size=(batch_size,latent_size))
-    dropout_noise = []
-    num_upsamples = int(math.log2(img_size)-2)
-    for i in range(num_upsamples):
-        dropout_noise.append(np.random.choice([1.0, 0.0], size=(batch_size, channels), replace=True, p=[1.0-alpha, alpha]))
-    return [latent_noise, *dropout_noise]
-
-def random_generator_input(batch_size, latent_dim, img_size):
-    null_input = np.zeros((batch_size, 1))
-    z_noise = np.random.normal(size=(batch_size,latent_dim))
+def random_generator_input(batch_size, latent_dim, start_size=(2,2), target_size=(64,64)):
+    dummy_input = np.zeros((batch_size, 1))
+    latent_noise = np.random.normal(size=(batch_size, latent_dim))
     noises = []
-    num_upsamles = int(math.log2(img_size)-2)
-    noises.append(np.random.normal(size=(batch_size, 4, 4, 1)))
-    for i in range(1,num_upsamles):
-        noises.append(np.random.normal(size=(batch_size,4*(2**i),4*(2**i),1)))
-        noises.append(np.random.normal(size=(batch_size,4*(2**(i)),4*(2**(i)),1)))
-    noises.append(np.random.normal(size=(batch_size, img_size, img_size, 1)))
+
+    width = start_size[0]
+    height = start_size[1]
+
+    num_upsamles = int(math.log2(target_size[0]/start_size[0]))
+    noises.append(np.random.normal(size=(batch_size, width, height, 1)))
+    for i in range(1, num_upsamles):
+        width = 2*width
+        height = 2*height
+        noises.append(np.random.normal(size=(batch_size, width, height, 1)))
+        noises.append(np.random.normal(size=(batch_size, width, height, 1)))
+    
+    noises.append(np.random.normal(size=(batch_size, target_size[0], target_size[1], 1)))
     return [null_input, z_noise, *noises]
